bcb802e61ce7d5f51703bb130d4dac40a6bc728e
Dipesh Jadhav <dipxsss@gmail.com>
2025-12-17 22:52:48 +0530

    Add OrdersPage component and related styles, update routing in App component

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { HomePage } from './pages/HomePage.jsx'
 import { Route, Routes } from 'react-router'
 import { CheckoutPage } from './pages/CheckoutPage.jsx'
+import { OrdersPage } from './pages/OrdersPage.jsx'
 import './App.css'
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
    <Routes>
     <Route index element={<HomePage/>}/>
      <Route path='/checkout' element={<CheckoutPage/>}/>
-    
+    <Route path='/ordersPage' element={<OrdersPage/>}/>
     </Routes>
   ) 
 }
