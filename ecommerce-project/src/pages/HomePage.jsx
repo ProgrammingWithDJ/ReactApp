b91d60bc4b2a492b28b4e31d7383d8b3547b752f
Dipesh Jadhav <dipxsss@gmail.com>
2025-12-21 10:47:10 +0530

    Add calculations

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import './HomePage.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from '../Components/Header.jsx';
+import { formatMoney } from '../utils/money.js';
 
 export function HomePage({ cart }) {
 
@@ -44,7 +45,7 @@ export function HomePage({ cart }) {
                                 </div>
 
                                 <div className="product-price">
-                                   ${(product.priceCents /100).toFixed(2) }
+                                    {formatMoney(product.priceCents)}
                                 </div>
 
                                 <div className="product-quantity-container">
