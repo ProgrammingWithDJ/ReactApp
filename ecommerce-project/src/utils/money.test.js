ecab8c37bdcf7e2daa4b4a52b5b2e592eafa20e1
Dipesh Jadhav <dipxsss@gmail.com>
2025-12-28 20:26:44 +0530

    create test suit

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -1,6 +1,23 @@
-import {it, expect} from 'vitest';
+import {it, expect, describe} from 'vitest';
 import { formatMoney } from './money.js';
 
-it('formats 1999 cents as $19.99', () => {
+describe('formatMoney', () => {
+
+    it('formats 1999 cents as $19.99', () => {
    expect(formatMoney(1999)).toBe('$19.99'); 
 });
+
+it('displays 2 decimal places for whole dollars', () => {
+    expect(formatMoney(5000)).toBe('$50.00');
+});
+
+it('formats 0 cents as $0.00', () => {
+    expect(formatMoney(0)).toBe('$0.00');
+});
+
+it('formats 50 cents as $0.50', () => {
+    expect(formatMoney(50)).toBe('$0.50');
+});
+
+ });
+
